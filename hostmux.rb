f7c23ca4d3c4877bdc2defbf4238e84add823f7d
John-Paul Bader <contact@smyck.org>
2016-08-24 17:20:54 +0200

    Update hostmux.rb

--- hostmux.rb
+++ hostmux.rb
@@ -1,9 +1,9 @@
 class Hostmux < Formula
   desc "Wrapper script for tmux to connect to a list of hosts via ssh"
   homepage "https://github.com/hukl/hostmux/"
-  url "https://github.com/hukl/hostmux/archive/1.1.0.tar.gz"
-  version "1.1.0"
-  sha256 "2852c3b27f2dd82a0311fa937244c3f932c2ed2084459f5710e6ed69bda79bcd"
+  url "https://github.com/hukl/hostmux/archive/1.1.1.tar.gz"
+  version "1.1.1"
+  sha256 "5dfccf25826217713a1739186dad90936a328b3b22220e0a55956ef8f656eb1c"
 
   depends_on "tmux"
 
